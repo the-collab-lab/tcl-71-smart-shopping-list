e3177b292129aaa326acd647cd5077e40b4910d4
cenkalp <cenk@bayzat.com>
2024-02-03 19:54:07 +0300

    firebase config filled.

--- src/api/config.js
+++ src/api/config.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'FILL_ME_IN',
-	authDomain: 'FILL_ME_IN',
+	apiKey: 'AIzaSyCnrz0zhsT7mih_K6t67dT2K6a5wcNaLzY',
+	authDomain: 'tcl-71-smart-shopping-list.firebaseapp.com',
 	databaseURL: 'FILL_ME_IN',
-	projectId: 'FILL_ME_IN',
-	storageBucket: 'FILL_ME_IN',
-	messagingSenderId: 'FILL_ME_IN',
-	appId: 'FILL_ME_IN',
+	projectId: 'tcl-71-smart-shopping-list',
+	storageBucket: 'tcl-71-smart-shopping-list.appspot.com',
+	messagingSenderId: '200403710630',
+	appId: '1:200403710630:web:1af41c1300a8751bde3a67',
 };
 
 // Initialize Firebase
