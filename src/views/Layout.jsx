56e5884ac9d53fc3069feb2b22f365c39200ea86
Umeh Amaka <amakaumeh01@gmail.com>
2024-02-04 19:34:58 +0100

    implement sign in and sign out

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 import './Layout.css';
 import { auth } from '../api/config.js';
-import {SignInButton, SignOutButton, useAuth} from "../api/useAuth.jsx";
+import { SignInButton, SignOutButton, useAuth } from '../api/useAuth.jsx';
 
 /**
  * TODO: The links defined in this file don't work!
@@ -13,14 +13,15 @@ import {SignInButton, SignOutButton, useAuth} from "../api/useAuth.jsx";
  */
 
 export function Layout() {
-
-	const user = useAuth()
+	const { user } = useAuth();
 
 	return (
 		<>
 			<div className="Layout">
 				<header className="Layout-header">
 					<h1>Smart shopping list</h1>
+					<p>{user?.displayName ? `Welcome ${user?.displayName}` : null}</p>
+					{!!user ? <SignOutButton /> : <SignInButton />}
 				</header>
 				<main className="Layout-main">
 					<Outlet />
