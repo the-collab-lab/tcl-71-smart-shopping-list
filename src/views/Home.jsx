e77c1e0778791650ea566c90ef6c9615c44bca9f
Céline <114222588+ocsiddisco@users.noreply.github.com>
2024-02-13 10:27:36 +0100

    add form to home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,40 @@
 import './Home.css';
 import { SingleList } from '../components';
+import { useState } from 'react';
+import { createList } from '../api';
 
 export function Home({ data, setListPath }) {
+	const [newList, setNewList] = useState('');
+
+	const handleSubmit = () => {
+		console.log('submitted');
+	};
+	const handleAddValue = (event) => {
+		const data = event.target.value;
+		setNewList(data);
+	};
+	const handleKeyPressed = (event) => {
+		if (event.key === 'Enter') {
+			handleSubmit();
+		}
+	};
+
 	return (
 		<div className="Home">
 			<p>
 				Hello from the home (<code>/</code>) page!
 			</p>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="new list name">Name new list</label>
+				<input
+					type="text"
+					id="new list name"
+					value={newList}
+					onChange={(event) => handleAddValue(event)}
+					onClick={(event) => handleKeyPressed(event)}
+				/>
+				<button type="submit">Confirm list</button>
+			</form>
 			<ul>
 				{data.map((list, i) => (
 					<SingleList
