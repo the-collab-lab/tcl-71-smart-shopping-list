cae051ea720447d991946559577efb6875fcd7f6
Céline <114222588+ocsiddisco@users.noreply.github.com>
2024-02-26 11:51:51 +0100

    add navigation to manage-list page

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
 import { ListItem } from '../components';
 import SearchList from '../components/SearchList';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 export function List({ data }) {
 	const [newList, setNewList] = useState([]);
 	const { path } = useParams();
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		setNewList(data);
@@ -18,8 +19,15 @@ export function List({ data }) {
 			</h2>
 			{data.length === 0 ? (
 				<div>
-					<p>Well done! You have created your a new list!</p>
-					<button id="addFirstItem">Add first item</button>
+					<p>Well done! You have created a new list!</p>
+					<p>
+						You can start adding some items and specify when you need to
+						purchase them.
+					</p>
+
+					<button id="addFirstItem" onClick={() => navigate('/manage-list')}>
+						Start adding items!
+					</button>
 				</div>
 			) : (
 				<div>
