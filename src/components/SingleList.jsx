93424571a90f355bf94d49a60f81a5a7a2807e5e
vivitt <viviviyanez@gmail.com>
2024-03-13 21:06:36 +0100

    add delete list button

--- src/components/SingleList.jsx
+++ src/components/SingleList.jsx
@@ -1,16 +1,32 @@
 import { useNavigate } from 'react-router-dom';
 import './SingleList.css';
+import { deleteList } from '../api/firebase';
 
-export function SingleList({ name, path, setListPath }) {
+export function SingleList({ name, path, setListPath, userId }) {
 	const navigate = useNavigate();
 	function handleClick() {
 		setListPath(path);
 		navigate(`/list/${path}`);
 	}
 
+	function handleDelete(user, listPath, listName) {
+		if (
+			window.confirm(`Do you really want to delete ${name.toUpperCase()} list?`)
+		) {
+			deleteList(user, listPath, listName);
+		}
+		return;
+	}
+
 	return (
 		<li className="SingleList">
 			<button onClick={handleClick}>{name}</button>
+			<button
+				className="List__delete-button"
+				onClick={() => handleDelete(userId, path, name)}
+			>
+				Delete List
+			</button>
 		</li>
 	);
 }
