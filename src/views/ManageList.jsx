b28be068c60155d70fef95689b4be80b57bcf631
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2024-04-01 00:58:09 +0100

    first styling of manageList.jsx

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -93,49 +93,68 @@ export function ManageList({ data, listPath, userId, userEmail }) {
 	}
 
 	return (
-		<div className="ManageList">
-			<p>
-				Hello from the <code>/manage-list</code> page!
-			</p>
-			<div className="ManageList__form">
-				<form method="post" onSubmit={handleSubmit}>
-					<label>
-						Add item
-						<input
-							type="text"
-							name="item"
-							onChange={() => setAddItemErrMessage('')}
-						></input>
-					</label>
-					<label htmlFor="time-select">When do I need it?</label>
-					<select name="time" id="time-select">
-						<option value="soon">Soon (within 7 days)</option>
-						<option value="soonIsh">Soon-ish (in 14 days)</option>
-						<option value="notSoon">Not soon (in 30 days)</option>
-					</select>
-					<button type="submit">Submit</button>
-				</form>
-				{addItemErrMessage !== '' && (
-					<ErrorMessage errorMessage={addItemErrMessage} />
-				)}
+		<div>
+			<div className="text-center flex flex-col py-2 min-w-96 justify-center items-center align-center mx-auto">
+				<h1 className="font-amiri text-4xl text-darkPurple p-8">{listPath}</h1>
 			</div>
-			<div className="ManageList__form">
-				<form method="post" onSubmit={sendInvite}>
-					<label htmlFor="email">
-						Share List with another user
-						<input
-							type="email"
-							name="email"
-							id="email"
-							onChange={() => setShareListErrMessage('')}
-						></input>
-					</label>
-					<button type="submit">Submit</button>
-				</form>
-				{shareListErrMessage !== '' && (
-					<ErrorMessage errorMessage={shareListErrMessage} />
-				)}
+
+			<div className="text-2xl py-8 md:w-2/4 w-3/4">
+				<p className="pb-12 text-darkPurple font-poppins">
+					Add new items and share your list with other users
+				</p>
 			</div>
+
+			<section className="mb-20">
+				<h2 className="text-lg sm:text-xl text-left text-darkPurple border-solid border-darkPurple border-b pb-2 mb-8">
+					ADD A NEW ITEM
+				</h2>
+				<div className="ManageList__form">
+					<form method="post" onSubmit={handleSubmit}>
+						<label>
+							Add Item
+							<input
+								type="text"
+								placeholder="Type a new item name"
+								name="item"
+								className="grow shrink bg-puurWhite border border-darkPurple rounded-md shadow-lg px-4 py-2 placeholder:text-darkPurple"
+								onChange={() => setAddItemErrMessage('')}
+							></input>
+						</label>
+						<label htmlFor="time-select">When do I need it?</label>
+						<select name="time" id="time-select">
+							<option value="soon">Soon (within 7 days)</option>
+							<option value="soonIsh">Soon-ish (in 14 days)</option>
+							<option value="notSoon">Not soon (in 30 days)</option>
+						</select>
+						<button type="submit">Submit</button>
+					</form>
+					{addItemErrMessage !== '' && (
+						<ErrorMessage errorMessage={addItemErrMessage} />
+					)}
+				</div>
+			</section>
+			<section className="mb-20">
+				<h2 className="text-lg sm:text-xl text-left text-darkPurple border-solid border-darkPurple border-b pb-2 mb-8">
+					SHARE LIST WITH ANOTHER USER
+				</h2>
+				<div className="ManageList__form">
+					<form method="post" onSubmit={sendInvite}>
+						<label htmlFor="email">
+							Share List with another user
+							<input
+								type="email"
+								name="email"
+								id="email"
+								onChange={() => setShareListErrMessage('')}
+							></input>
+						</label>
+						<button type="submit">Submit</button>
+					</form>
+					{shareListErrMessage !== '' && (
+						<ErrorMessage errorMessage={shareListErrMessage} />
+					)}
+				</div>
+			</section>
 		</div>
 	);
 }
