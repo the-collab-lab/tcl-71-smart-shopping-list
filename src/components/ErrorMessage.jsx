26b1662055c9c41de63abf649a89a809568ece3c
Borja Martí <borjamarti@outlook.com>
2024-04-04 00:55:23 +0200

    implemented visual differentiation to Message and ErrorMessage components

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,6 +1,8 @@
 const ErrorMessage = ({ errorMessage }) => {
 	return (
-		<p className="text-alertRed mt-10 text-base sm:text-lg">{errorMessage}</p>
+		<p className="italic text-alertRed mt-10 text-base sm:text-lg border-alertRed border-2 rounded-md px-4 py-2 shadow-lg">
+			{errorMessage}
+		</p>
 	);
 };
 
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,6 +1,8 @@
 const Message = ({ message }) => {
 	return (
-		<p className="text-darkPurple mt-10 text-base sm:text-lg">{message}</p>
+		<p className="italic text-darkPurple mt-10 text-base sm:text-lg border-darkPurple border-2 rounded-md px-4 py-2 shadow-lg">
+			{message}
+		</p>
 	);
 };
 
