a48141e825f9c8c304ccff9c9c4e465f92bcb85a
vivitt <viviviyanez@gmail.com>
2024-04-03 18:48:09 +0200

    delete unused vars in ListButtons

--- src/components/ListButtons.jsx
+++ src/components/ListButtons.jsx
@@ -9,18 +9,8 @@ const ListButtons = (props) => {
 			'text-base sm:text-lg text-darkPurple flex items-center justify-center  rounded-md bg-lightGrey border text-darkPurple transition ease-in-out hover:bg-hoverPurple hover:text-offWhite',
 	};
 
-	const iconVariants = {
-		purple: 'fa-inverse',
-		white: '',
-	};
-
-	const textVariants = {
-		purple: 'font-poppins',
-		white: ' font-poppins',
-	};
-
 	return (
-		<div className="grid sm:grid-cols-3 grid-cols-2 gap-4 py-6 text-base sm:text-lg">
+		<div className="grid sm:grid-cols-3 grid-cols-2 gap-4 py-6 text-base sm:text-lg font-poppins">
 			<button
 				className={`sm:col-span-2  px-4 py-2 gap-6 shadow-lg ${buttonVariants[props.colorAdd]}`}
 				onClick={() => navigate('/manage-list')}
